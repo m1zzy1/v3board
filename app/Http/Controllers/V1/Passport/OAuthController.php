f4a6ce8337a8052da11a59fece2c5241a03547b5
codeman857 <kim24smel@gmail.com>
2025-09-05 21:01:40 +0800

    feat: 新增Google/Telegram 登录20

--- app/Http/Controllers/V1/Passport/OAuthController.php
+++ app/Http/Controllers/V1/Passport/OAuthController.php
@@ -328,8 +328,8 @@ class OAuthController extends Controller
      */
     public function handleTelegramLogin(Request $request)
     {
-        // 1. 生成25位唯一的 hash 值
-        $hash = Helper::generateTelegramLoginCode(25);
+        // 1. 生成21位唯一的 hash 值
+        $hash = Helper::generateTelegramLoginCode(21);
         
         // 2. 将 hash 值存储到缓存中，设置过期时间（例如5分钟）
         $cacheKey = CacheKey::get('TELEGRAM_LOGIN_HASH', $hash);
